ad5f74d870893a2fd19430b19f803d39733471c2
Leonardo2696 <leonardoastargo@gmail.com>
2026-06-26 19:09:10 -0300

    Actualizacion de codigo

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import minimist from "minimist";
+import { PelisCollection } from "./models"; // Asegúrate de importar tu clase
+
+type Search = {
+  title?: string;
+  tag?: string;
+};
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
 }
 
-function main() {
+async function main() {
   const params = parseaParams(process.argv.slice(2));
+  const pelisCollection = new PelisCollection();
 
-  console.log(params);
+  if (params._[0] === "search") {
+    const searchOptions: Search = {};
+    if (params.title) searchOptions.title = params.title;
+    if (params.tag) searchOptions.tag = params.tag;
+
+    const pelisFiltradas = await pelisCollection.search(searchOptions);
+    console.log(pelisFiltradas);
+  } else {
+    console.log("Comando no reconocido. Usa 'get' o 'search'.");
+  }
 }
 
 main();
